2811df2171e640859db17efacaf95e5d4c65fdcf
SaranKaewchuay <saranyeen@gmail.com>
2023-03-22 13:11:18 +0700

    start

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/hello/{name}")
+def read_name(name: str = None):
+    return {"hello": name}
+
+
+@app.get("/test")
+def read_name():
+    return {"Hello": "World2"}
+
+
+handler = Mangum(app)
