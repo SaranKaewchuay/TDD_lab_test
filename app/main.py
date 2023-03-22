6bae74e48c11a7ddf2c6ce827537f2abdfbf947e
SaranKaewchuay <saranyeen@gmail.com>
2023-03-22 13:40:36 +0700

    edit post

--- app/main.py
+++ app/main.py
@@ -12,10 +12,12 @@ app = FastAPI()
 def read_name(name: str = None):
     return {"hello": name}
 
-# @app.post("/callname")
-# def read_name(name: str = None):
-#     return {"hello": {name}}
+@app.post("/callname")
+def create_name():
+    return {"hello": "Saran_yeen"}
 
+
+# create_name("Saran_yeen")
 
 # @app.get("/test")
 # def read_name():
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -5,14 +5,12 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/callname/saran")
+def test_call_name():
+    response = client.get("/callname/Saran Kaewchuay")
     assert response.status_code == 200
-    assert response.json() == {"hello": "saran"}
+    assert response.json() == {"hello": "Saran Kaewchuay"}
     
-# def test_hello():
-#     response = client.get("/test")
-#     assert response.status_code == 200
-#     assert response.json() == {"Hello": "World2"}
-
-
+def test_post_name():
+    response = client.post("/callname")
+    assert response.status_code == 200
+    assert response.json() == {"hello": "Saran_yeen"}
