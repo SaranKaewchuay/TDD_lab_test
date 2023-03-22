0269681eab5795684e46bd591cc36a72d4d6dcc2
SaranKaewchuay <saranyeen@gmail.com>
2023-03-22 13:17:26 +0700

    start

--- app/main.py
+++ app/main.py
@@ -3,22 +3,23 @@ from mangum import Mangum
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
 
-@app.get("/hello/{name}")
+@app.get("/callname/{name}")
 def read_name(name: str = None):
-    return {"hello": name}
+    return {"hello": {name}}
+
+@app.post("/callname")
+def read_name(name: str = None):
+    return {"hello": {name}}
 
 
-@app.get("/test")
-def read_name():
-    return {"Hello": "World2"}
+# @app.get("/test")
+# def read_name():
+#     return {"Hello": "World2"}
 
 
 handler = Mangum(app)
